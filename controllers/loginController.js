c1e3e6db8062a382dc5412c9bd80c36966ba750c
dhollinden <dave.hollinden@ithaka.org>
2018-08-23 08:05:02 -0400

    Add signup page, routes and functions for signup_post and signup_get

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -10,6 +10,8 @@ const axios = require('axios');
 const bcrypt = require('bcrypt-nodejs');
 
 
+// ------------- Passport -------------
+
 // configure passport.js to use the local strategy
 passport.use(new LocalStrategy(
     { usernameField: 'email' },
@@ -42,26 +44,76 @@ passport.deserializeUser((id, done) => {
 });
 
 
-// display login page on GET
-exports.login_get= function (req, res, next) {
+// ------------- callbacks -------------
 
-    res.render('login', {title: 'Login'});
+// login page GET
+exports.login_get = function (req, res, next) {
+
+    res.render('login', {title: 'Log In'});
 
 };
 
-// handle login page on POST
-exports.login_post= function (req, res, next) {
+
+// login page POST
+exports.login_post = function (req, res, next) {
 
     passport.authenticate('local', (err, user, info) => {
         console.log(err, user, info);
-        if(info) {return res.render('login', {title: 'Login', message: info.message})}
+        if(info) {return res.render('login', {title: 'Log In', message: info.message})}
         if (err) { return next(err); }
         if (!user) { return res.redirect('/login'); }  // what is this for?
         req.login(user, (err) => {
             if (err) { return next(err); }
-            return res.render('login', {title: 'Login', message: 'Login was successful'});
+            return res.render('login', {title: 'Log In', message: 'Login was successful'});
         })
     })(req, res, next);
 
 };
 
+
+// signup page GET
+exports.signup_get = function (req, res, next) {
+
+    res.render('login', {title: 'Sign Up'});
+
+};
+
+
+// signup page POST
+exports.signup_post = function (req, res, next) {
+
+    const password = req.body.password;
+    axios.get(`http://localhost:5000/users?email=${req.body.email}`)
+        .then(res_axios => {
+            const user = res_axios.data[0];
+            if (user) {
+                // email exists in db so return to signup page
+                return res.render('login', {title: 'Sign Up Error', message: `The email address ${req.body.email} has already been registered`})
+            } else {
+
+                // hash the password with bcrypt and create random id
+                const saltRounds = 10;
+                const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(saltRounds));
+                const id = Math.random().toString(36).substring(2, 9);
+
+                // store data in the db.json using axios.post()
+                axios.post('http://localhost:5000/users', {
+
+                    id: id,
+                    email: req.body.email,
+                    password: hash
+                })
+                    .then(function (response) {
+                        return res.render('login', {title: 'Sign Up Success', message: `Success! You have been registered with the email address ${req.body.email}`})
+                    })
+                    .catch(error => next(error));
+            }
+        })
+        .catch(error => next(error));
+
+
+
+
+// then call req.login() with the user object you’ve created.
+
+};
--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,10 @@ router.get('/login', login_controller.login_get);
 // POST request for login page
 router.post('/login', login_controller.login_post)
 
+// GET request for signup page
+router.get('/signup', login_controller.signup_get);
+
+// POST request for login page
+router.post('/signup', login_controller.signup_post)
+
 module.exports = router;
