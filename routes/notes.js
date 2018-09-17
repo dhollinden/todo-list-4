25304728be27f9b8b4c75c7c85395bff08adbc6b
dhollinden <dave.hollinden@ithaka.org>
2018-09-17 04:47:31 -0400

    Reformat and comment notes.js router

--- routes/notes.js
+++ routes/notes.js
@@ -6,32 +6,40 @@ const note_controller = require('../controllers/noteController');
 
 // ROUTES
 
-// GET request for notes home page
+
+// Notes section GET
 router.get('/', note_controller.index);
 
-// POST request for individual note
+
+// Display note POST
 router.post('/', note_controller.note_detail_post);
 
-// GET request for creating a note
-router.get('/create', note_controller.note_create_get);
 
-// POST request for creating a note
+// Create note
+// GET
+router.get('/create', note_controller.note_create_get);
+// POST
 router.post('/create', note_controller.note_create_post);
 
-// GET request for updating a note
-router.get('/:id/update', note_controller.note_update_get);
 
-// POST request for updating a note
+// Update note
+// GET
+router.get('/:id/update', note_controller.note_update_get);
+// POST
 router.post('/:id/update', note_controller.note_update_post);
 
-// GET request for deleting a note
-router.get('/:id/delete', note_controller.note_delete_get);
 
-// POST request for deleting a note
+// Delete note
+// GET
+router.get('/:id/delete', note_controller.note_delete_get);
+// POST
 router.post('/:id/delete', note_controller.note_delete_post);
 
-// GET request for individual note
+
+
+// Display note GET
 router.get('/:id', note_controller.note_detail_get);
+
 
 module.exports = router;
 
